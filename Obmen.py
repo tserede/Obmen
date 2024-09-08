5faffa6323652352f0322febb09d5f6f1d07ad21
tserede <kasy.06@mail.ru>
2024-09-08 13:34:10 +0300

    Добавлен второй выпадающий список для выбора базовой валюты

--- Obmen.py
+++ Obmen.py
@@ -1,4 +1,5 @@
 from idlelib.editor import keynames
+from sys import builtin_module_names
 
 import requests
 import json
@@ -17,23 +18,27 @@ cur={"RUB": "Российский рубль",
 "AED":"Дирхам ОАЭ",
 "CHF":"Швейцарский франк",
 "CAD":"Канадский доллар",
+"USD":"Американский доллар"
 }
 def update_c_label(event):
-    code=combobox.get()
+    code=t_combobox.get()
     name=cur[code]
     c_label.config(text=name)
 
 def exchange():
-    code=combobox.get()
-    if code:
+    t_code=t_combobox.get()
+    b_code=b_combobox.get()
+    if t_code and b_code:
         try:
-            responce=requests.get("http://open.er-api.com/v6/latest/USD")
+
+            responce=requests.get(f"http://open.er-api.com/v6/latest/{b_code}")
             responce.raise_for_status()
             data=responce.json()
-            if code in data["rates"]:
-                exchange_rates=data["rates"][code]
-                c_name=cur[code]
-                mb.showinfo("Курс обмена",f"Курс:{exchange_rates:.2f} {c_name} за 1 доллар ")
+            if t_code in data["rates"]:
+                exchange_rates=data["rates"][t_code]
+                t_name=cur[t_code]
+                b_name=cur[b_code]
+                mb.showinfo("Курс обмена",f"Курс:{exchange_rates:.2f} {t_name} за 1 {b_name} ")
             else:
                 mb.showerror("Ошибка", f'Валюта {code} не найдена')
         except Exception as e:
@@ -43,20 +48,26 @@ def exchange():
 
 window=Tk()
 window.title("Курсы обмена валют")
-window.geometry("360x180")
+window.geometry("360x300")
 
-Label(text="Выберите код валюты").pack(padx=10,pady=10)
+Label(text="Базовая валюта").pack(padx=10,pady=10)
 
-combobox=ttk.Combobox(values=list(cur.keys()))
-combobox.pack(padx=10,pady=10)
-combobox.bind("<<ComboboxSelected>>", update_c_label)
+b_combobox=ttk.Combobox(values=list(cur.keys()))
+b_combobox.pack(padx=10,pady=10)
+
+
+Label(text="Целевая валюта").pack(padx=10,pady=10)
+
+t_combobox=ttk.Combobox(values=list(cur.keys()))
+t_combobox.pack(padx=10,pady=10)
+t_combobox.bind("<<ComboboxSelected>>", update_c_label)
 
 c_label=ttk.Label()
 c_label.pack(padx=10,pady=10)
 
 # entry=Entry()
 # entry.pack(padx=10,pady=10)
-Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10,pady=10)
+Button(text="Получить курс обмена", command=exchange).pack(padx=10,pady=10)
 
 window.mainloop()
 
